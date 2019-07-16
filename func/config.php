407785f0b387d9c4b9afe5d1a2b8d56bf05be0c9
soiqualang <soiqualang.chentreu@gmail.com>
2019-07-16 11:03:46 +0700

    add func

---
+++ func/config.php
@@ -0,0 +1,16 @@
+<?php
+//session_save_path($_SERVER['DOCUMENT_ROOT'] . '/tmp');
+//ini_set('session.gc_probability', 1);
+	define("PG_DB"  , "harmonicatabs");
+	define("PG_HOST", "localhost"); 
+	define("PG_USER", "postgres");
+	define("PG_PORT", "5433"); 
+	define("TABLE",   "tabs");
+	define("PG_PASS","2679191");
+  /* define("PG_DB"  , "angiang_v1");
+  define("PG_HOST", "localhost"); 
+  define("PG_USER", "postgres");
+  define("PG_PORT", "5432"); 
+  define("TABLE",   "tabs");
+  define("PG_PASS","TrungT@mV13nTh@m"); */
+?>
